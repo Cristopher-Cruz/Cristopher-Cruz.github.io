4b3d8fa48418ff84c52e5191abe343d008c3dca4
Chriscruzz17 <christophercruz0236@gmail.com>
2023-05-08 00:58:36 -0400

    Changed light/dark mode switch

--- js/global.js
+++ js/global.js
@@ -10,134 +10,29 @@ const navLinks = document.querySelectorAll('nav a').forEach(link => {
 
 // ----------------> Light/Dark mode
 
-window.addEventListener('load', () => {
-  const LOCAL_VARIABLES = Object.freeze({
-    ALPHA: '@darklightmode:alpha',
-    MODE: '@darklightmode:mode'
-  });
+// Get the checkbox element and the HTML body
+const checkbox = document.getElementById('slider');
+const body = document.body;
 
-  feather.replace();
-
-  function applyBrightness() {
-    let alpha = localStorage.getItem(LOCAL_VARIABLES.ALPHA);
-
-    document
-      .querySelector('#mask')
-      .style
-      .backgroundColor = `rgba(0, 0, 0, ${alpha})`;
+// Check the saved theme preference, if any
+const theme = localStorage.getItem('theme');
+if (theme) {
+  body.classList.add(theme);
+  if (theme === 'dark-mode') {
+    checkbox.checked = true;
   }
+}
 
-  function downBrightness() {
-    let alpha = parseFloat(localStorage.getItem(LOCAL_VARIABLES.ALPHA));
-
-    if(alpha > 0) {
-      alpha -= 0.1;
-    }
-
-    localStorage.setItem(LOCAL_VARIABLES.ALPHA, alpha.toFixed(1));
-
-    applyBrightness();
-  }
-
-  function upBrightness() {
-    let alpha = parseFloat(localStorage.getItem(LOCAL_VARIABLES.ALPHA));
-
-    if(alpha < 0.7) {
-      alpha += 0.1;
-    }
-
-    localStorage.setItem(LOCAL_VARIABLES.ALPHA, alpha.toFixed(1));
-
-    applyBrightness();
-  }
-
-  function useDarkMode() {
-    localStorage.setItem(LOCAL_VARIABLES.MODE, 'dark');
-
-    document
-      .querySelector('#light-mode')
-      .classList
-      .remove('mode-selected');
-
-    document
-      .querySelector('#dark-mode')
-      .classList
-      .add('mode-selected');
-
-    document
-      .querySelector('body')
-      .classList
-      .add('dark-mode');
-  }
-
-  function useLightMode() {
-    localStorage.setItem(LOCAL_VARIABLES.MODE, 'light');
-
-    document
-      .querySelector('#dark-mode')
-      .classList
-      .remove('mode-selected');
-
-    document
-      .querySelector('#light-mode')
-      .classList
-      .add('mode-selected');
-
-    document
-      .querySelector('body')
-      .classList
-      .remove('dark-mode');
-  }
-
-  let mode = localStorage.getItem(LOCAL_VARIABLES.MODE);
-  useDarkMode();
-  // useLightMode();
-
-
-  if(mode && mode === 'dark') {
-    useDarkMode();
+// Toggle the theme and save the preference
+function toggleTheme() {
+  if (checkbox.checked) {
+    body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark-mode');
   } else {
-    useLightMode();
+    body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light-mode');
   }
-
-  let alpha = localStorage.getItem(LOCAL_VARIABLES.ALPHA);
-
-  if(!alpha) {
-    localStorage.setItem(LOCAL_VARIABLES.ALPHA, 0.0);
-    useDarkMode();
-    // useLightMode();
-    
-  }
-
-  applyBrightness(); 
-
-  document
-    .querySelector('#settings')
-    .addEventListener('click', () => {
-      document
-        .querySelector('#settings-block')
-        .classList
-        .toggle('hide');
-    });
-
-  document
-    .querySelector('#light-mode')
-    .addEventListener('click', useLightMode);
-
-  document
-    .querySelector('#dark-mode')
-    .addEventListener('click', useDarkMode);
-
-  document
-    .querySelector('#down-brightness')
-    .addEventListener('click', upBrightness);
-
-  document
-    .querySelector('#up-brightness')
-    .addEventListener('click', downBrightness);
-});
-
-
+}
 // ----------------> slideshow  
 
 
@@ -177,6 +72,7 @@ window.addEventListener('load', () => {
 
     }
   }
+  
 //prev/next functionality
 
   links = document.querySelectorAll('.slideshow-container a');
