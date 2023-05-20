17162d8280c939142828ceb0ba133aaee7bfffa2
Chriscruzz17 <christophercruz0236@gmail.com>
2023-05-20 11:37:42 -0400

    Update responsive UI

--- js/global.js
+++ js/global.js
@@ -184,6 +184,21 @@ function setTheme() {
 })();
 
 
+// Check if the device is mobile
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Remove scroll snap behavior on mobile devices
+if (isMobile()) {
+  var container = document.querySelector('.container');
+  container.style.scrollSnapType = 'none';
+  var sections = document.querySelectorAll('section');
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].style.scrollSnapAlign = 'none';
+  }
+}
+
 
 
 
